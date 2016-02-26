5b124b79e21605444fbf326c0b7938477f337e31
Nicole <nicole.j.paz@gmail.com>
2016-02-25 18:24:12 -0800

    Add process method to call_logs that runs all of the methods stored in call_logs, then destroys the record.

--- app/models/call_log.rb
+++ app/models/call_log.rb
@@ -1,2 +1,8 @@
 class CallLog < ActiveRecord::Base
+  def self.process
+    CallLog.find_each do |log|
+      log.location.constantize.send(log.location_method)
+      log.destroy
+    end
+  end
 end
--- spec/models/call_log_spec.rb
+++ spec/models/call_log_spec.rb
@@ -1,5 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe CallLog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:noaa_sync) do
+    NOAASync.new
+  end
+
+  let(:dataset_response) do
+    create_response(62, true)
+  end
+
+  let(:location_response) do
+    create_response(107, true)
+  end
+
+  let(:result_count) do
+    {}
+  end
+
+  before do
+    stub_response(dataset_response, 'datasets')
+    stub_response(location_response, 'locations')
+
+    dataset_results = dataset_response[:results].map { |result| result[:id] }
+
+    max = 0
+    dataset_results.each do |result|
+      min = max
+      max += 2
+
+      results = location_response[:results][min...max] || []
+      response = { metadata: { resultset: { count: results.length } }, results: results }
+      result_count[result] = results.map { |res| res[:id] }
+
+      stub_request(:get, "http://ncdc.noaa.gov/cdo-web/api/v2/locations/?datasetid=#{result}&limit=1000").
+        with(headers: {'Token'=>Rails.application.secrets.NOAA_API_KEY}).
+        to_return(status: 200, body: response.to_json, headers: {:content_type => 'application/json'})
+    end
+  end
+
+  context 'process' do
+    it 'iterates through the call logs and reruns the import method' do
+      DataSet.populate
+      Location.populate
+      CallLog.create(location: 'DataSetLocation', location_method: 'populate_joins')
+      CallLog.process
+
+      expect(CallLog.count).to eq(0)
+      expect(DataSetLocation.count).to eq(107)
+
+      first_set = DataSet.first
+      first_results = result_count[first_set.identifier]
+
+      third_set = DataSet.third
+      third_results = result_count[third_set.identifier]
+
+      expect(first_set.locations.pluck(:identifier)).to eq(first_results)
+      expect(third_set.locations.pluck(:identifier)).to eq(third_results)
+    end
+  end
 end
